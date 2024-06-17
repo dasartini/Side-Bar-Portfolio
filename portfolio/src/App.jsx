a9e8e0a8771fc447ffcb6f8f592f6647f450a0bb
dasartini <adriansartinid@gmail.com>
2024-06-17 20:22:30 +0100

    added social icons with links

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,11 +1,14 @@
 
 import './App.css'
 import Sidebar from '../components/Sidebar'
+import { Routes, Route } from 'react-router-dom'
 function App() {
 
   return (
     <div>
+  
     <Sidebar/>
+ 
     </div>
   )
 }
